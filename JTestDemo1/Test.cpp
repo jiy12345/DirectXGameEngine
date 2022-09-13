fd334288f5e08f52eb05f7e913f7cff40ea618e1
jiy12367@gmail.com <jiy12367@gmail.com>
2022-09-13 20:37:28 +0900

    test: Creating a test project using a library

---
+++ JTestDemo1/Test.cpp
@@ -0,0 +1,19 @@
+#include "Test.h"
+#include "stdafx.h"
+#include "JGameEngine.h"
+
+#define WINDOW_SIZE_X 1024
+#define WINDOW_SIZE_Y 768
+#define WINDOW_NAME L"DirectXGameEngine"
+
+int APIENTRY wWinMain(
+	HINSTANCE	hInstance,
+	HINSTANCE	hPrevInstance,
+	LPWSTR		lpCmdLine,
+	int			nCmdShow) {
+
+	I_Window.setWindow(hInstance, WINDOW_NAME, WINDOW_SIZE_X, WINDOW_SIZE_Y);
+	I_GameEngine.run();
+
+	return 0;
+}
---
+++ JTestDemo1/Test.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "JGameEngine.h"
+class Test : public JGameEngine
+{
+public:
+};
+
+
