3cd5dd0a98d6032b138cb290e58915460c958484
jiy12367@gmail.com <jiy12367@gmail.com>
2022-09-21 22:18:25 +0900

    feat: Modify the hlsl code to output the color of the texture as it is

--- JTestDemo1/DefaulTextureShader.hlsl
+++ JTestDemo1/DefaulTextureShader.hlsl
@@ -19,13 +19,23 @@ VS_out VS(VS_in input)
 	output.t = input.tex;
 	return output;
 }
+
+SamplerState MeshTextureSampler
+{
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = Wrap;
+	AddressV = Wrap;
+};
+Texture2D    g_txTex : register(t0);
+
 struct PS_input
 {
 	float4 p : SV_POSITION;
 	float4 c : COLOR0;
 	float2 t : TEXCOORD0;
 };
-float4 PS(	PS_input input) : SV_Target
+float4 PS(PS_input input) : SV_Target
 {
-	return input.c;
+	float4 vColor = g_txTex.Sample(MeshTextureSampler, input.t);
+	return vColor;
 }
