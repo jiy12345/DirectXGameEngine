23c426a61a22c45a2523ff1433da6e722ba52882
jiy12367@gmail.com <jiy12367@gmail.com>
2022-09-21 20:20:08 +0900

    feat: Creating a Default Object Class
    - make the default object class also plays the role that the existing JShader class used to play

---
+++ JTestDemo1/JBaseObject.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "stdafx.h"
+#include "JDevice.h"
+#include "JShaderManager.h"
+
+struct SimpleVertex
+{
+	JVector<3> p;
+	JVector<4> c;
+	JVector<2> t;
+};
+
+class JBaseObject
+{
+public:
+	std::wstring				m_wstrVSName = L"DefaultShapeMask.hlsl";
+	std::wstring				m_wstrPSName = L"DefaultShapeMask.hlsl";
+	std::string					m_strVSFuncName = "VS";
+	std::string					m_strPSFuncName = "PS";
+	ID3D11Buffer*				m_pVertexBuffer;
+	ID3D11Buffer*				m_pIndexBuffer;
+	ID3D11InputLayout*			m_pVertexLayout;
+	std::vector<SimpleVertex>   m_VertexList;
+	std::vector<DWORD>			m_IndexList;
+public:
+	void				setVSName(std::wstring wstrVSName);
+	void				setPSName(std::wstring wstrPSName);
+	void				setVSFuncName(std::string strVSFuncName);
+	void				setPSFuncName(std::string strPSFuncName);
+public:
+	virtual void		setVertexData();
+	virtual void		setIndexData();
+	virtual HRESULT		createVertexBuffer();
+	virtual HRESULT		createIndexBuffer();
+	virtual HRESULT     createVertexLayout();
+	virtual void		updateVertexBuffer();
+public:
+	virtual bool		init();
+	virtual bool		frame();
+	virtual bool		render();
+	virtual bool		release();
+protected:
+	bool				preRender();
+	bool				postRender();
+public:
+	virtual				 ~JBaseObject();
+};
