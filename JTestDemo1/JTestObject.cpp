5c811625beff71812a18ae8843683573f2d2ebe8
DESKTOP-1GGFRU5\jiy12345 <jiy12367@gmail.com>
2022-09-29 03:17:55 +0900

    fix: Resolve the error that textures did not printed properly
    - correct the vertex order

--- JTestDemo1/JTestObject.cpp
+++ JTestDemo1/JTestObject.cpp
@@ -5,11 +5,11 @@ bool JTestObject::frame()
     JVector<2> vPos = m_rtArea.m_vLeftTop;
     if (I_Input.GetKey('W'))
     {
-        vPos[1] += -1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
+        vPos[1] += +1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
     }
     if (I_Input.GetKey('S'))
     {
-        vPos[1] += 1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
+        vPos[1] += -1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
     }
     if (I_Input.GetKey('A'))
     {
@@ -17,7 +17,7 @@ bool JTestObject::frame()
     }
     if (I_Input.GetKey('D'))
     {
-        vPos[0] += 1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
+        vPos[0] += +1.0f * I_Timer.m_fElapseTimer * m_fSpeed;
     }
     m_rtArea.m_vLeftTop = vPos;
 
@@ -27,16 +27,16 @@ bool JTestObject::frame()
 void JTestObject::updateVertexBuffer()
 {
     nCube<2> rtNDC = getNDC();
-    m_VertexList[0].p = { rtNDC.m_vLeftTop[0], rtNDC.m_vLeftTop[1], 0.0f };
+    m_VertexList[0].p = { rtNDC.m_vLeftTop[0], rtNDC.m_vLeftTop[1] + rtNDC.m_vSize[1], 0.0f };
     m_VertexList[0].t = { m_rtUV.m_vLeftTop[0], m_rtUV.m_vLeftTop[1] };
 
-    m_VertexList[1].p = { rtNDC.m_vLeftTop[0] + rtNDC.m_vSize[0],  rtNDC.m_vLeftTop[1],  0.0f };
+    m_VertexList[1].p = { rtNDC.m_vLeftTop[0] + rtNDC.m_vSize[0],  rtNDC.m_vLeftTop[1] + rtNDC.m_vSize[1],  0.0f };
     m_VertexList[1].t = { m_rtUV.m_vLeftTop[0] + m_rtUV.m_vSize[0], m_rtUV.m_vLeftTop[1] };
 
-    m_VertexList[2].p = { rtNDC.m_vLeftTop[0],  rtNDC.m_vLeftTop[1] + rtNDC.m_vSize[1], 0.0f };
+    m_VertexList[2].p = { rtNDC.m_vLeftTop[0],  rtNDC.m_vLeftTop[1], 0.0f };
     m_VertexList[2].t = { m_rtUV.m_vLeftTop[0], m_rtUV.m_vLeftTop[1] + m_rtUV.m_vSize[1] };
 
-    m_VertexList[3].p = { rtNDC.m_vLeftTop[0] + rtNDC.m_vSize[0],  rtNDC.m_vLeftTop[1] + rtNDC.m_vSize[1], 0.0f };
+    m_VertexList[3].p = { rtNDC.m_vLeftTop[0] + rtNDC.m_vSize[0],  rtNDC.m_vLeftTop[1], 0.0f };
     m_VertexList[3].t = { m_rtUV.m_vLeftTop[0] + m_rtUV.m_vSize[0], m_rtUV.m_vLeftTop[1] + m_rtUV.m_vSize[1] };
 
     I_Device.m_pImmediateContext->UpdateSubresource(
--- JTestDemo1/JTestObject.h
+++ JTestDemo1/JTestObject.h
@@ -7,7 +7,7 @@ public:
 	nCube<2>		m_rtUV;
 	nCube<2>		m_rtArea;
 public:
-	float			m_fSpeed;
+	float			m_fSpeed = 1000.0f;
 public:
 	bool			frame() override;
 	virtual void	updateVertexBuffer() override;
@@ -15,8 +15,12 @@ public:
 	nCube<2>		getNDC();
 public:
 	JTestObject() {
-		m_rtUV.Set({ 0, 0 }, { 1, 1 });
-		m_rtArea.Set({ 0, 0 }, { 100, 100 });
+		m_wstrTextureName = L"../data/sprites/pilot.png";
+		//m_rtUV.Set({ 24 / 811.0f, 0 / 988.0f }, { 22 / 811.0f + 0.05f, 15 / 988.0f + 0.05f });
+		m_rtUV.Set({ 0 / 988.0f, 24 / 811.0f }, { 15 / 988.0f, 22 / 811.0f });
+		// m_rtUV.Set({ 0, 0 }, { 1, 1 });
+		
+		m_rtArea.Set({ 0, 0 }, { m_rtUV.m_vSize[0]* 3000, m_rtUV.m_vSize[1] * 3000 });
 	}
 };
 
